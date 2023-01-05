beabb31a117bbd9da39bd7f470c64a71f6882a88
zouggagh-hamid <31382709+azul100@users.noreply.github.com>
2023-01-05 07:11:09 +0100

    Antivirus
    azul100/usb/Antivirus

---
+++ IAgentCtlCharacter.vb
@@ -0,0 +1,28 @@
+﻿
+
+
+
+
+
+
+Namespace AgentObjects
+    Interface IAgentCtlCharacter
+
+        Property Width As Integer
+
+        Property Height As Integer
+
+        Sub MoveTo(leftpos As Long, toppos As Long)
+
+        Sub Show()
+
+        Sub Hide()
+
+        Sub Speak(text As String)
+
+    End Interface
+
+    Class Agent
+
+    End Class
+End Namespace
